52911f50a220044b8c8942920e0b31d314a1a87d
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-02-12 01:03:53 +0900

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+from collections import Counter
+import re
+
+# ウェブページのURL
+url = 'https://news.yahoo.co.jp/topics/top-picks'
+
+# ページの内容を取得
+response = requests.get(url)
+
+with open('test.html', 'w', encoding='utf-8') as file:
+    file.write(response.text)
