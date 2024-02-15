d3bc2abc28e827d458835e8f703e2441596cda21
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-02-15 21:48:25 +0900

    f

--- src/test/test.py
+++ src/test/test.py
@@ -3,12 +3,11 @@ from bs4 import BeautifulSoup
 from collections import Counter
 import re
 
-# ウェブページのURL
 url = 'https://news.yahoo.co.jp/topics/top-picks'
 
 # ページの内容を取得
 response = request.urlopen(url)
 soup = BeautifulSoup(response)
 
-test_one = soup.find('script', type='application/ld+json')
-print(test_one.text)
+test_one = soup.find('nav', class_='sc-bBXrwG immAzn')
+print(soup)
