a65ddb56c980a57603474d23a78e75566faf5dde
ogiek0628 <e225755@ie.u-ryukyu.ac.jp>
2024-04-28 22:00:58 +0900

    jsonファイルを出力

--- main/src/components/App.jsx
+++ main/src/components/App.jsx
@@ -1,14 +1,59 @@
-// App.jsx
-import React from 'react';
-import VerticalList from './VerticalList';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const App = () => {
-  const items = ["Item 1", "Item 2", "Item 3"];
+  const [jsonData, setJsonData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const result = await axios('http://127.0.0.1:5000/news');
+      setJsonData(result.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const renderData = () => {
+    if (jsonData) {
+      return (
+        <div>
+          {Object.entries(jsonData).map(([key, value]) => (
+            <div key={key}>
+              <strong>{key}: </strong>
+              {value}
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return <p>Loading...</p>;
+    }
+  };
   
+  
+  
+  
+
   return (
     <div>
-      <h1>Todays NEWS List</h1>
-      <VerticalList items={items} />
+      <h1>NEWS LIST</h1>
+      <button>reload</button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        renderData()
+      )}
     </div>
   );
 };
