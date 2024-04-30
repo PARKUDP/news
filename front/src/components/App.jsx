7d9e7e383cc9c1bb33276f4c6c19282c8fd36d1d
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-05-01 02:36:21 +0900

    Docker

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-//import '../style/App.css';
+import '../style/App.css';
 
 const App = () => {
   const [jsonData, setJsonData] = useState(null);
