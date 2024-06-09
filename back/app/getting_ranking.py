db88870bf96c9a3cf1cb58c049727a21d4e68452
ogiek0628 <e225755@ie.u-ryukyu.ac.jp>
2024-06-09 18:01:00 +0900

    appファイルにてスクレイピング実行ファイルとflaskファイルを分割し整理

--- back/app/getting_ranking.py
+++ back/app/getting_ranking.py
@@ -8,12 +8,16 @@ import re
 from collections import ChainMap
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS  #study/src/main.pyより
+import scraping
 
 
 #CORSを記述
 app = Flask(__name__)
 CORS(app)
 
+
+
+# yahooニュース表示の設定
 @app.route('/news_yahoo')
 def json_news_data():
     # トピックス一覧1~100件までのURL
@@ -23,10 +27,10 @@ def json_news_data():
     url_four = 'https://news.yahoo.co.jp/topics/top-picks?page=4'
 
     # トピック一覧1~100件までのデータ
-    data_one = data_inquire(url_one)
-    data_two = data_inquire(url_two)
-    data_three = data_inquire(url_three)
-    data_four = data_inquire(url_four)
+    data_one = scraping.yahoo_news.data_inquire(url_one)
+    data_two = scraping.yahoo_news.data_inquire(url_two)
+    data_three = scraping.yahoo_news.data_inquire(url_three)
+    data_four = scraping.yahoo_news.data_inquire(url_four)
 
     result_data = ChainMap(data_one, data_two, data_three, data_four)
     merged_data = {}
@@ -36,37 +40,37 @@ def json_news_data():
     
     return jsonify(merged_data) ######## marged_dataをjson形式でreturnに変更
 
-def data_inquire(url):
-    response = request.urlopen(url)
-    soup = BeautifulSoup(response, features="lxml")
-
-    script_tag_data = soup.find_all('script')  # すべてのscriptタグを取得
-    prepare_data = script_tag_data[3] # ニュースのタイトルが入っているデータ
-
-    # ニュースのタイトルを取得
-    news_title = re.findall(r'"title":".*?"', str(prepare_data))  # ニュースのタイトルを取得
-    news_title = [re.sub(r'"title":"', '', i) for i in news_title]  # ニュースのタイトルの前の文字列を削除
-    news_title = [re.sub(r'"', '', i) for i in news_title]  # ニュースのタイトルの後の文字列を削除
-
-    # ニュースのURLを取得(処理)
-    news_url = re.findall(r'"url":".*?"', str(prepare_data))  # ニュースのURLを取得
-    news_url = [re.sub(r'"url":"', '', i) for i in news_url]  # ニュースのURLの前の文字列を削除
-    news_url = [re.sub(r'"', '', i) for i in news_url]  # ニュースのURLの後の文字列を削除
 
 
-    # ニュースのタイトルとURLの必要ない情報を削除
-    news_title.remove('主要トピックス一覧')
-    news_title.remove('アクセスランキング')
-    news_title.remove('コメントランキング')
-    news_title.remove('トピックス（主要）')
 
-    for url in news_url:
-        if url.find('https://news.yahoo.co.jp/pickup/') == -1:
-            news_url.remove(url)
+# cnnニュース表示の設定
+@app.route('/news_cnn')
+def json_news_data():
+    # トピックス一覧1~90件までのURL
+    url_one = 'https://www.cnn.co.jp/archives/'
+    url_two = 'https://www.cnn.co.jp/archives/2/'
+    url_three = 'https://www.cnn.co.jp/archives/3'
+    url_four = 'https://www.cnn.co.jp/archives/4'
+    url_five = 'https://www.cnn.co.jp/archives/5'
+    url_six = 'https://www.cnn.co.jp/archives/6'
+    url_seven = 'https://www.cnn.co.jp/archives/7'
 
-    # ニュースのタイトルとURLをdictで保存する。
-    result = dict(zip(news_title, news_url))
-    return result
+    # トピック一覧1~90件までのデータ
+    data_one = scraping.cnn_news.data_inquire(url_one)
+    data_two = scraping.cnn_news.data_inquire(url_two)
+    data_three = scraping.cnn_news.data_inquire(url_three)
+    data_four = scraping.cnn_news.data_inquire(url_four)
+    data_five = scraping.cnn_news.data_inquire(url_five)
+    data_six = scraping.cnn_news.data_inquire(url_six)
+    data_seven= scraping.cnn_news.data_inquire(url_seven)
+
+    result_data = ChainMap(data_one, data_two, data_three, data_four, data_five, data_six, data_seven)
+    merged_data = {}
+    
+    for d in result_data.maps:
+        merged_data.update(d)
+    
+    return jsonify(merged_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
